ee401d6753c39ea580794768339d835963984f88
Piotr Majcher <piotr.majcher94@gmail.com>
2017-11-23 13:52:00 +0100

    Ready

--- scalar_product.js
+++ scalar_product.js
@@ -1,26 +1,25 @@
 /**
  * 
  */
+var Double = Java.type("java.lang.Double");
+var String = Java.type("java.lang.String");
+var JOptionPane = Java.type("javax.swing.JOptionPane");
+
 var sayHello = function(name) {
   print('Hello, ' + name + '!');
   return 'hello from javascript';
 };
 
 var multi01 = function(a, b) {
-	var Double = Java.type("java.lang.Double");
 	var result = new Double(0.0);
-	
 	var i;
 	for (i = 0; i < a.length; i++) {
 		result += a[i] * b[i];
 	}
-	print('result: ' + result);
 	return result;
 }
 
 var multi02 = function(object, b) {
-	var Double = Java.type("java.lang.Double");
-	
 	var len = object.getA().length;
 	var result = new Double(0.0);
 	
@@ -32,8 +31,17 @@ var multi02 = function(object, b) {
 	return result;
 }
 
-var multi03 = function(object) {
-	var Double = Java.type("java.lang.Double");
-	// Pobrać dane z okna od użytkownika
+var multi03 = function(object) {	
+	var input1 = JOptionPane.showInputDialog("Multi03: Input first double values array separated with commas");
+	var input2 = JOptionPane.showInputDialog("Multi03: Input second double values array separated with commas");
+	
+	var stringInput1 = new String(input1);
+	var array1 = stringInput1.split(',');
+	var stringInput2 = new String(input1);
+	var array2 = stringInput1.split(',');
+
+	object.setA(array1);
+	object.setB(array2);
+	
 	object.multi04();
 }
--- src/main/java/com/piotrmajcher/jsinjava/App.java
+++ src/main/java/com/piotrmajcher/jsinjava/App.java
@@ -2,10 +2,13 @@ package com.piotrmajcher.jsinjava;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.jar.JarInputStream;
+import java.util.Arrays;
 
-import javax.script.*;
-import javax.swing.JDialog;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.swing.JOptionPane;
 /**
  * Hello world!
  *
@@ -15,20 +18,23 @@ public class App
     public static void main( String[] args )
     {	
     	ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-    	
+    
     	try {
     		engine.eval(new FileReader("scalar_product.js"));
 			ScalarProduct scalarProduct = new ScalarProduct((Invocable) engine);
+			
 			Double[] a = {1.0, 2.0, 3.0};
 			Double[] b = {4.0, 5.0, 6.0};
 			Double res = scalarProduct.multi01(a, b);
-			System.out.println("multi01: " + res);
-			scalarProduct.setA(a);
+			System.out.println("multi01: " + "\na " + Arrays.toString(a) + "\nb " + Arrays.toString(b) + "\nres " +res);
+			
+			Double[] a2 = {10.0, 11.0, 12.0};
+			scalarProduct.setA(a2);
 			res = scalarProduct.multi02(b);
-			System.out.println("multi02: " + res);
+			System.out.println("multi02: "+ "\na " + Arrays.toString(scalarProduct.getA()) + "\nb " + Arrays.toString(b) + "\nres "+ res);
 			scalarProduct.setB(b);
 			scalarProduct.multi03();
-			System.out.println("multi03: " + scalarProduct.getC());
+			System.out.println("multi03: " + Arrays.toString(scalarProduct.getA()) + "\nb " + Arrays.toString(scalarProduct.getB()) + "\nres " + scalarProduct.getC());
 			
 		} catch (ScriptException e) {
 			// TODO Auto-generated catch block
@@ -40,6 +46,5 @@ public class App
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        System.out.println( "Hello World!" );
     }
 }
